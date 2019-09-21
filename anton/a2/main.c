65955f20f5d9dd6e46e25c6a839915405d06c63c
Ditt Namn <du@example.com>
2019-09-21 13:53:51 +0200

    lol

---
+++ anton/a2/main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+   char *x = argv[1];
+   char *y = argv[2];
+   int d = atoi(argv[3]);
+
+   int image_size, n_threads;
+   if (x[1] == 't') {
+        image_size = ;:wq
+        b = y;
+    } else {
+        a = y;
+        b = x;
+    }
+
+    char *A = malloc(sizeof(char) * (strlen(a) - 2));
+    char *B = malloc(sizeof(char) * (strlen(b) - 2));
+
+    strncpy(A, a + 2, strlen(a) - 2);
+    strncpy(B, b + 2, strlen(b) - 2);
+
+    int A_int = atoi(A);
+    int B_int = atoi(B);
+
+    printf("A is %ld and B is %ld\n", A_int, B_int);
+
+}
+
+
+    return 0;
+}
+
