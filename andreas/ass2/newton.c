becb1e2d30bb2e285067fe5159667229ecb72938
someone <someone@someplace.com>
2019-09-25 11:46:11 +0200

    faster program in sweden

--- andreas/ass2/newton.c
+++ andreas/ass2/newton.c
@@ -12,16 +12,14 @@
 
 const double TOL = 0.001;
 double dt;
-unsigned short int ** attractors; 
-unsigned int ** convergences;
 char **convergences_strings; 
 char **attractors_strings;
 unsigned short int DEGREE, N_THREADS;
 unsigned int LINES;
-double complex *roots;
-char *COLORS[] = {"9 0 0 ", "0 9 0 ", "9 5 3 ", "9 9 0 ", "0 9 9 ", "0 5 5 ", "0 5 0 ", "5 0 5 ", "0 0 5 ", "10 0 10 ", "0 0 10 "};
+double complex * restrict roots;
+char* restrict COLORS[] = {"9 0 0 ", "0 9 0 ", "9 5 3 ", "9 9 0 ", "0 9 9 ", "0 5 5 ", "0 5 0 ", "5 0 5 ", "0 0 5 ", "8 0 8 ", "0 0 8 "};
 //pre-format strings for speed
-char *ITERATIONS[] = {"00 00 00 ", "01 01 01 ", "02 02 02 ", "03 03 03 ", "04 04 04 ", "05 05 05 ", "06 06 06 ", "07 07 07 ", "08 08 08 ", "09 09 09 ",
+char* restrict ITERATIONS[] = {"00 00 00 ", "01 01 01 ", "02 02 02 ", "03 03 03 ", "04 04 04 ", "05 05 05 ", "06 06 06 ", "07 07 07 ", "08 08 08 ", "09 09 09 ",
                       "10 10 10 ", "11 11 11 ", "12 12 12 ", "13 13 13 ", "14 14 14 ", "15 15 15 ", "16 16 16 ", "17 17 17 ", "18 18 18 ", "19 19 19 ",
                       "20 20 20 ", "21 21 21 ", "22 22 22 ", "23 23 23 ", "24 24 24 ", "25 25 25 ", "26 26 26 ", "27 27 27 ", "28 28 28 ", "29 29 29 ",
                       "30 30 30 ", "31 31 31 ", "32 32 32 ", "33 33 33 ", "34 34 34 ", "35 35 35 ", "36 36 36 ", "37 37 37 ", "38 38 38 ", "39 39 39 ",
@@ -58,64 +56,26 @@ int main(int argc, char *argv[]){
     LINES = atoi(l_string);
     DEGREE = atoi(argv[3]);
 
-    if(DEGREE == 1){
-        
-        char name1[50], name2[50];
-
-        sprintf(name1, "%s%d%s", "newton_attractors_x", DEGREE, ".ppm");
-        sprintf(name2, "%s%d%s", "newton_convergence_x", DEGREE, ".ppm");
-
-        FILE *attr_ptr = fopen(name1, "w");
-        FILE *conv_ptr = fopen(name2, "w");
-
-        fprintf(attr_ptr, "P3\n%d %d\n10\n", LINES, LINES);
-        fprintf(conv_ptr, "P3\n%d %d\n100\n", LINES, LINES);
-
-
-        char *attr_string = malloc(sizeof(char)*6*LINES*LINES);
-        char *conv_string = malloc(sizeof(char)*12*LINES*LINES);
-
-        for(unsigned int i = 0; i < LINES; i++){
-            for(unsigned int j = 0, k1=0, k2=0; j < LINES; j++, k1+=9, k2+=6){
-
-                memcpy(conv_string + k1, ITERATIONS[1], 9);
-                memcpy(attr_string + k2, COLORS[1], 6); 
-            }                          
-        }
-
-        fwrite(attr_string, sizeof(char), 6*LINES*LINES, attr_ptr);    
-        fwrite(conv_string, sizeof(char), 12*LINES*LINES, conv_ptr);
-        fclose(attr_ptr);
-        fclose(conv_ptr);
-        return 0;
-
-    }
 
     free(t_string); free(l_string); 
     dt = 4.0/LINES;
     roots = malloc(sizeof(double complex)*(DEGREE+1));
     
-    for(int d = 0; d < DEGREE; d++)
+    for(unsigned short int d = 0; d < DEGREE; d++)
         roots[d+1] = cexp(2*PI*I*(double)d/DEGREE);
     roots[0] = 0;
 
-
-    attractors = malloc(sizeof(unsigned short int*)*LINES);
     attractors_strings = malloc(sizeof(char *)*LINES);
-    convergences = malloc(sizeof(unsigned int*)*LINES);
     convergences_strings = malloc(sizeof(char *)*LINES);
     
     for(unsigned int i = 0; i < LINES; i++){
-        attractors[i] = malloc(sizeof(unsigned short int)*LINES);
-        attractors[i][LINES-1] = 11; //11 means this line not finished
-        attractors_strings[i] = malloc(sizeof(char)*9*LINES);
-        convergences[i] = malloc(sizeof(unsigned int)*LINES);
-        convergences_strings[i] = malloc(sizeof(char)*15*LINES);
+        attractors_strings[i] = malloc(sizeof(char)*6*LINES+1);
+        attractors_strings[i][LINES*6] = 'N'; //means this line not yet finished
+        convergences_strings[i] = malloc(sizeof(char)*9*LINES);
     }
 
     pthread_t newtoner[N_THREADS];
     pthread_t writer;
-
     
     for(unsigned short int i = 0; i < N_THREADS; i++){
         unsigned short int *a = malloc(sizeof(unsigned short int));
@@ -126,17 +86,16 @@ int main(int argc, char *argv[]){
 
     
     pthread_join(writer, NULL);
+    return 0;
 }
 
-void *newton_fun(void *arg){ 
+void *newton_fun(void* restrict arg){ 
     
     unsigned short int offset = *(int *)arg;
     free(arg);
     
-
     for(unsigned int i = offset; i < LINES; i+=N_THREADS){
         
-
         for(unsigned int j = 0, k1 = 0, k2 = 0; j < LINES; k1+=9, k2+=6){
             
             double complex z = (-2+dt*j) + (2-dt*i)*I; 
@@ -144,45 +103,38 @@ void *newton_fun(void *arg){
             unsigned int iter = 0;
             double complex zconj, root;
             double a, b, normsq, root_a, root_b;
-            
-            
+                   
             while(1){
 
                 a = creal(z);
                 b = cimag(z);
                 normsq = a*a+b*b;
 
-                if(!(normsq >= (TOL+1)*(TOL+1) || normsq <= (1-TOL)*(1-TOL))){ //if normsq too large or too small then cant be close to root
+                if(!(normsq >= (TOL+1)*(TOL+1)) && !( normsq <= (1-TOL)*(1-TOL))){ //if normsq too large or too small then cant be close to root
                     
-
                     for(unsigned short int d = 1; d <= DEGREE; d++){
                         
                         root = roots[d];
                         root_a = creal(root);
                         root_b = cimag(root);
                         if( (a-root_a)*(a-root_a)+(b-root_b)*(b-root_b) < TOL*TOL){
-                            convergences[i][j] = iter;
-                            attractors[i][j] = d;
                                                         
                             if(iter > 99) 
                                 iter = 99;
-
+                            
                             memcpy(convergences_strings[i] + k1, ITERATIONS[iter], 9);
                             memcpy(attractors_strings[i] + k2, COLORS[d], 6); 
-                            
+                                                   
                             goto end;
-
                         }
                     }                   
                 }
               
-                if(normsq < TOL*TOL || a > 10000000000 || b > 10000000000){
-                    convergences[i][j] = iter;
-                    attractors[i][j] = 0;
+                else if(normsq < TOL*TOL || a > 10000000000 || b > 10000000000){
                                
                     if(iter > 99) 
                         iter = 99;
-
+                    
                     memcpy(convergences_strings[i] + k1, ITERATIONS[iter], 9);
                     memcpy(attractors_strings[i] + k2, COLORS[0], 6);
 
@@ -193,6 +145,9 @@ void *newton_fun(void *arg){
                 iter++;
                 switch(DEGREE){
 
+                    case 1:
+                        z = 1.0;
+                        break;
                     case 2:
                         z = 1.0/DEGREE*(z*(DEGREE-1)+zconj);                       
                         break;
@@ -229,10 +184,9 @@ void *newton_fun(void *arg){
             end:   
             j++;  //need statement after goto label    
         }
-
+        attractors_strings[i][LINES*6] = 'Y'; //now this line is done
     }
     return NULL;
-
 }
 
 void *write_fun(void *arg){
@@ -255,17 +209,18 @@ void *write_fun(void *arg){
        
     for(unsigned int i = 0; i < LINES;){
         
-        if(attractors[i][LINES-1] == 11){ //wait until finished
+        
+        if(attractors_strings[i][LINES*6] == 'N'){ //wait until finished
             nanosleep(&sleepvalue, NULL);
             continue;
         }
 
-        for(; i < LINES && attractors[i][LINES-1] != 11; i++){
+        for(; i < LINES && attractors_strings[i][LINES*6] == 'Y' ; i++){
        
             fwrite(attractors_strings[i], sizeof(char), 6*LINES, attr_ptr);    
             fwrite("\n", sizeof(char), 1, attr_ptr);  
                
-            fwrite(convergences_strings[i], sizeof(char), 12*LINES, conv_ptr);
+            fwrite(convergences_strings[i], sizeof(char), 9*LINES, conv_ptr);
             fwrite("\n", sizeof(char), 1, conv_ptr);
                     
         }       
